f9dc3d06aa6b734b060fb5437d300b0be493b8a9
012xx <night.fox.045@gmail.com>
2025-01-08 00:20:42 +0900

    Update go.mod and go.sum files with new dependencies

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"github.com/joho/godotenv"
 	"log"
 	"os"
 
@@ -9,11 +10,35 @@ import (
 )
 
 func main() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal(".env ファイルの読み込みに失敗しました")
+	}
+
 	channelSecret := os.Getenv("LINE_CHANNEL_SECRET")
 	channelToken := os.Getenv("LINE_CHANNEL_TOKEN")
-	toUserID := os.Getenv("LINE_TO_USER_ID")
-}
+	toUserID := os.Getenv("LINE_CHANNEL_ID")
 
-if channelSecret == "" || channelToken == "" || toUserID == "" {
-	log.Fatal("環境変数 LINE_CHANNEL_SECRET, LINE_CHANNEL_TOKEN, LINE_TO_USER_ID を設定してください")
+	if channelSecret == "" || channelToken == "" || toUserID == "" {
+		log.Fatal("環境変数 LINE_CHANNEL_SECRET, LINE_CHANNEL_TOKEN, LINE_CHANNEL_ID を設定してください")
+	}
+
+	bot, err := linebot.New(channelSecret, channelToken)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if len(os.Args) < 2 {
+		fmt.Println("使用方法: go run main.go \" MTGが始まったよ！\"")
+		return
+	}
+
+	messeage := os.Args[1]
+
+	_, err = bot.PushMessage(toUserID, linebot.NewTextMessage(messeage)).Do()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("メッセージを送信しました:", messeage)
 }
